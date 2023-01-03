f1c94478dc9df821ca9fe7c985af7bcec0da46bd
vimalravat <vimalravat6@gmail.com>
2023-01-03 20:59:41 +0000

    My homework week 18

---
+++ src/main/java/io/swagger/petstore/constants/Path.java
@@ -0,0 +1,17 @@
+package io.swagger.petstore.constants;
+
+public class Path {
+    /*
+     * This is base path for student api
+     */
+   // public static final String USER = "/";
+
+    /*
+     * This is base path for authenticate api
+     */
+    /*public static final String AUTHENTICATE = "/authenticate";
+
+    public static final String ACCOUNT = "/account";*/
+
+
+}
